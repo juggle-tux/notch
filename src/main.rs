ca7759577e0d7240e5c6159b53d02aa718b0958b
Walter Kuppens <reshurum@gmail.com>
2016-05-17 23:22:34 -0700

    Separated graphics functions from interconnect

--- src/main.rs
+++ src/main.rs
@@ -9,18 +9,17 @@ use std::path::Path;
 
 mod cpu;
 mod memory;
+mod graphics;
 mod interconnect;
+mod vm;
 
 fn main() {
     // TODO: Replace unwrap.
     let rom_file_name = env::args().nth(1).unwrap();
     let rom = read_bin(rom_file_name);
 
-    // Create a clean cpu state and virtual machine. The CPU must have a shorter
-    // lifetime than the virtual machine in order to use it's resources.
-    let interconnect = interconnect::Interconnect::new(rom);
-    let mut cpu = cpu::Cpu::new(interconnect);
-    cpu.run();
+    let mut vm = vm::VirtualMachine::new(rom);
+    vm.run();
 }
 
 fn read_bin<P: AsRef<Path>>(path: P) -> Vec<u8> {
